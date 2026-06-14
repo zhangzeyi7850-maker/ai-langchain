8a5a5dfbc666f88c9077e26982ef76de8b59b68a
张泽依 <zhangzeyi@starlinke.com>
2026-06-14 11:35:03 +0800

    fix: model模块

--- src/models/models.controller.ts
+++ src/models/models.controller.ts
@@ -6,22 +6,25 @@ import { ModelsService } from './models.service';
 export class ModelsController {
   constructor(private readonly modelsService: ModelsService) {}
 
-  // chat接口
+  // chat接口 普通调用
   @Post('chat')
   baseChat(@Body() { message }: { message: string }) {
     return this.modelsService.baseChat(message);
   }
 
+  // 系统提示词
   @Post('chat-system')
   chatSystem(@Body() { system, message }: { system: string; message: string }) {
     return this.modelsService.chatSystem({ system, message });
   }
 
+  // 流式输出
   @Post('chat-stream')
   chatStream(@Body() { message }: { message: string }, @Res() res: Response) {
     return this.modelsService.chatStream(message, res); // 把res也传过去
   }
 
+  // pipeline StringOutputParser 方式调用，适合复杂的多轮对话场景
   @Post('chat-pipeline')
   chatWithParser(@Body() { message }: { message: string }) {
     // 这里可以定义一个更复杂的pipeline调用，适合多轮对话或者需要多个模型协同工作的场景
