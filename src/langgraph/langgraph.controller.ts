537ef1bc54b5dd88b9f80a3a27fe2d8b793d3d37
张泽依 <zhangzeyi@starlinke.com>
2026-07-13 11:49:48 +0800

    fix: 添加controller

--- src/langgraph/langgraph.controller.ts
+++ src/langgraph/langgraph.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Post, Get, Body, Param } from '@nestjs/common'
 import { LanggraphService } from './langgraph.service'
 import { ArticleService } from './article.service'
+import { ReactAgentService } from './class-2-service/react-agent/react-agent.service'
+import { RoutingService } from './class-2-service/routing/routing.service'
+import { ParallelService } from './class-2-service/parallel/parallel.service'
 
 @Controller('langgraph')
 export class LanggraphController {
   constructor(
     private readonly langgraphService: LanggraphService,
-    private readonly articleService: ArticleService
+    private readonly articleService: ArticleService,
+    private readonly reactAgentService: ReactAgentService,
+    private readonly routingService: RoutingService,
+    private readonly parallelService: ParallelService
   ) {}
 
   /* 工作流1 无记忆简单回答 */
@@ -33,5 +39,21 @@ export class LanggraphController {
   @Post('article')
   processArticle(@Body() body: { article: string }) {
     return this.articleService.process(body.article)
+  }
+
+  /* 第二章接口 */
+  @Post('react-chat')
+  reactChat(@Body() body: { threadId: string; message: string }) {
+    return this.reactAgentService.chat(body.threadId, body.message).then((answer) => ({ answer }))
+  }
+
+  @Post('route')
+  route(@Body() body: { input: string }) {
+    return this.routingService.handle(body.input)
+  }
+
+  @Post('parallel')
+  parallel(@Body() body: { task: string }) {
+    return this.parallelService.parallelChat(body.task)
   }
 }
